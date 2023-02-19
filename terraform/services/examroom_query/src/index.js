0e03839bf2a0b626901bebf8f7890e549c153f5e
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-20 00:09:54 +0800

    npm: use got instead of axios
    cause it's shorter

--- terraform/services/examroom_query/src/index.js
+++ terraform/services/examroom_query/src/index.js
@@ -1,20 +1,12 @@
-import axios from "axios";
+import got from "got";
 
 export const handler = async (event, context) => {
-  console.log(event);
-  console.log(context);
   const xh = event.queryString.xh;
-  if (xh == undefined) {
-    throw new Error("请检查是否已经登录")
-  }
-  return new Promise((resolve, _) => {
-    axios.get(`http://kskw.ahu.edu.cn/bkcx.asp?xh=${xh}`).then(res => {
-      resolve({
-        statusCode: 200,
-        body: res.data
-      });
-    }).catch(err => {
-      throw new Error(err)
-    });
+  if (xh == undefined) throw new Error("请检查是否已经登录");
+  return new Promise(async (resolve, _) => {
+    try {
+      const result = await got(`http://kskw.ahu.edu.cn/bkcx.asp?xh=${xh}`).text();
+      resolve({ statusCode: 200, body: result });
+    } catch (err) { throw new Error(err) }
   })
-};
+}
