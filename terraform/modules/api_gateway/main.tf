bfe9368062619e9f86468c7c9d573b4a8a5780a8
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-23 20:24:58 +0800

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -6,19 +6,8 @@ terraform {
   }
 }
 
-resource "tencentcloud_dnspod_domain_instance" "dnspod_domain_instance" {
-  domain = var.domain
-}
-
-resource "tencentcloud_dnspod_record" "host_dnspod_record" {
-  record_type = "A"
-  record_line = "默认"
-  value       = var.host
-  domain      = tencentcloud_dnspod_domain_instance.dnspod_domain_instance.domain
-}
-
 resource "tencentcloud_api_gateway_service" "api_gateway_service" {
-  protocol     = "http"
+  protocol     = "https"
   net_type     = ["OUTER"]
   service_name = "api_gateway_service"
 }
@@ -27,12 +16,13 @@ resource "tencentcloud_dnspod_record" "api_dnspod_record" {
   sub_domain  = "api"
   record_line = "默认"
   record_type = "CNAME"
-  domain      = tencentcloud_dnspod_domain_instance.dnspod_domain_instance.domain
+  domain      = var.domain
   value       = tencentcloud_api_gateway_service.api_gateway_service.outer_sub_domain
 }
 
 resource "tencentcloud_api_gateway_custom_domain" "custom_domain" {
-  protocol       = "http"
+  protocol       = "https"
+  certificate_id = var.certificate_id
   net_type       = "OUTER"
   service_id     = tencentcloud_api_gateway_service.api_gateway_service.id
   default_domain = tencentcloud_api_gateway_service.api_gateway_service.outer_sub_domain
